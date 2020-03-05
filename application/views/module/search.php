1377fcdabaa13274113d9ce59f269c078cfc9207
Baoavin <baoavin@gmail.com>
2020-03-05 17:39:20 +0700

    update

--- application/views/module/search.php
+++ application/views/module/search.php
@@ -1,9 +1,11 @@
 <div class="widget widget-search">
 	<form action="<?=site_url("{$this->util->slug($this->router->fetch_class())}")?>" method="get" accept-charset="utf-8">
 		<div class="wrap-search input-group">
-			<input type="text" name="search_text" id="input" class="form-control ipt-search" value="" placeholder="Search">
+			<input type="text" name="search_text" id="input" class="form-control ipt-search" value="" placeholder="Enter search content">
 			<div class="input-group-append">
-				<button type="submit" class="btn-search">&rarr;</button>
+				<div class="btn-checkfee pl-4">
+					<input type="submit" class="btn btn-danger btn-search" value="SEARCH">
+				</div>
 			</div>
 			<!-- <button type="submit" class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></button> -->
 		</div>
